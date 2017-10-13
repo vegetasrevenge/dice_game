eb75bc5cfc1ae819a6965e391d6d4bed216f18cf
vegetasrevenge <cjedwards233@yahoo.com>
2017-10-13 10:57:22 -0500

    refactor to play again

--- src/main/java/com/challenge/dicegame/DiceGameApplication.java
+++ src/main/java/com/challenge/dicegame/DiceGameApplication.java
@@ -1,5 +1,6 @@
 package com.challenge.dicegame;
 
+import com.sun.tools.doclint.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,37 +12,11 @@ public class DiceGameApplication {
 
 	public static void main(String[] args) {
 
-
-
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("*********** Let's play a dice game! ************\n");
-
-		System.out.println("How many dice will be in your cup?");
 		Cup cup = new Cup();
-		Integer userInput = sc.nextInt();
-		System.out.println("That's " + userInput + " dice. Here we go!\n");
 
-			for (int i = 0; i < userInput; i++) {
-				if (i % 3 == 0) {
-					LoadedDie loadedDie = new LoadedDie(1);
-					loadedDie.setRollResult();
-					cup.dieList.add(loadedDie);
-				} else {
-					Die die = new Die();
-					die.setRollResult();
-					cup.dieList.add(die);
-				}
-			}
+		cup.play();
 
-		System.out.println("----------Roll Results---------");
-		cup.getDieList().stream().forEach(x -> System.out.println("Rolling " + x.getClass().getSimpleName() + ": " + x.getRollResult()));
-
-
-		System.out.println("Total of rolls: " + cup.getDieList().stream().mapToInt(x -> x.getRollResult()).sum());
-		System.out.println("-------------------------------");
-
-
+		cup.playAgain();
 	}
 }
 
